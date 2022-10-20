ae5be6b4a251277943216a1761e17a62abd04c1f
Marcin Świderek <marcin.swiderek@htdevelopers.com>
2022-10-20 19:56:40 +0200

    Add boilerplate

---
+++ screens/TabTwoScreen.tsx
@@ -0,0 +1,36 @@
+import {Image, StyleSheet} from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Suczka rasowa</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text style={styles.subtitle}>100% czysta rasowo</Text>
+      <Image source={{ uri: 'https://i.redd.it/nj63dcte47201.jpg'}} style={{ width: 250, height: 350 }} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: 'normal',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
