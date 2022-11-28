a6cd202780341b1638c72282fce1ee5bfa151a19
Jonasz Potoniec <jonasz.potoniec@gmail.com>
2022-11-28 19:22:41 +0100

    Fixed navigation @marcin 😡
    Co-authored-by: Konrad Nowak <konrad0025@users.noreply.github.com>
    Co-authored-by: Nilx12 <Nilx12@users.noreply.github.com>

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,44 +1,113 @@
-import { FontAwesome } from '@expo/vector-icons';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+/**
+ * If you are not familiar with React Navigation, refer to the "Fundamentals" guide:
+ * https://reactnavigation.org/docs/getting-started
+ *
+ */
+ import { FontAwesome } from '@expo/vector-icons';
+ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+ import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+ import { createNativeStackNavigator } from '@react-navigation/native-stack';
+ import * as React from 'react';
+ import { ColorSchemeName, Pressable } from 'react-native';
+ 
+ import Colors from '../constants/Colors';
+ import useColorScheme from '../hooks/useColorScheme';
+ import ModalScreen from '../screens/ModalScreen';
+ import NotFoundScreen from '../screens/NotFoundScreen';
+ import TabOneScreen from '../screens/TabOneScreen';
+ import TabTwoScreen from '../screens/TabTwoScreen';
+ import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
+ import LinkingConfiguration from './LinkingConfiguration';
+ import {LoginScreen} from '../screens/LoginScreen';
+ import {RegisterScreen} from '../screens/RegisterScreen';
+ import {ForgotPasswordScreen} from '../screens/ForgotPasswordScreen';
+ import { useAuthentication } from '../hooks/useAuthentication';
+ import {YourDogsListScreen} from '../screens/YourDogsListScreen';
+ import {AddDogScreen} from '../screens/AddDogScreen';
+ import {SwipeDogsScreen} from '../screens/SwipeDogsScreen';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import * as React from 'react';
-import {LoginScreen} from '../screens/LoginScreen';
-import {RegisterScreen} from '../screens/RegisterScreen';
-import {ForgotPasswordScreen} from '../screens/ForgotPasswordScreen';
-import { useAuthentication } from '../hooks/useAuthentication';
-import {YourDogsListScreen} from '../screens/YourDogsListScreen';
-import {AddDogScreen} from '../screens/AddDogScreen';
-import {SwipeDogsScreen} from '../screens/SwipeDogsScreen';
-
-export default function Navigation() {
-  const { user } = useAuthentication();
-  return user ? <UserStack /> : <AuthStack />;
-}
-
-const Drawer = createDrawerNavigator();
-const Stack = createNativeStackNavigator();
-
-function UserStack() {
-  return (
-    <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home" useLegacyImplementation={true}>
-        <Drawer.Screen name="YourDogsListScreen" component={YourDogsListScreen} options={{ headerShown: true }} />
-        <Drawer.Screen name="SwipeDogsScreen" component={SwipeDogsScreen} options={{ headerShown: true }} />
-        <Drawer.Screen name="AddDogScreen" component={AddDogScreen} options={{ headerShown: true }} />
-      </Drawer.Navigator>
-    </NavigationContainer>
-  )
-}
-
-function AuthStack() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{ headerShown: false }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+ 
+ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
+   const { user } = useAuthentication();
+   return user ? <UserStack colorScheme={colorScheme} /> : <AuthStack colorScheme={colorScheme} />;
+ }
+ 
+ /**
+  * A root stack navigator is often used for displaying modals on top of all other content.
+  * https://reactnavigation.org/docs/modal
+  */
+ const Stack = createNativeStackNavigator<RootStackParamList>();
+ 
+ function UserStack({ colorScheme }: { colorScheme: ColorSchemeName }) {
+   return (
+     <NavigationContainer
+       linking={LinkingConfiguration}
+       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+     <Stack.Navigator>
+       <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
+       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+       <Stack.Screen name="Login" component={LoginScreen} />
+       <Stack.Screen name="Register" component={RegisterScreen} options={{ title: 'login!' }} />
+       <Stack.Screen name="AddDogScreen" component={AddDogScreen} options={{ headerShown: false }} />
+       <Stack.Screen name="SwipeDogsScreen" component={SwipeDogsScreen} options={{ headerShown: false }} />
+       <Stack.Group screenOptions={{ presentation: 'modal' }}>
+         <Stack.Screen name="Modal" component={ModalScreen} />
+       </Stack.Group>
+     </Stack.Navigator>
+     </NavigationContainer>
+   );
+ }
+ 
+ function AuthStack({ colorScheme }: { colorScheme: ColorSchemeName }) {
+   return (
+     <NavigationContainer
+       linking={LinkingConfiguration}
+       theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+     <Stack.Navigator>
+       <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+       <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} options={{ headerShown: false }} />
+     </Stack.Navigator>
+     </NavigationContainer>
+   );
+ }
+ 
+ /**
+  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
+  * https://reactnavigation.org/docs/bottom-tab-navigator
+  */
+ const Drawer = createDrawerNavigator<RootTabParamList>();
+ 
+ function BottomTabNavigator() {
+   const colorScheme = useColorScheme();
+ 
+   return (
+     <Drawer.Navigator
+       initialRouteName="TabOne">
+       <Drawer.Screen
+         name="SwipeDogsScreen"
+         component={SwipeDogsScreen}
+         options={{
+           title: 'SwipeDogsScreen',
+         }}
+       />
+       <Drawer.Screen
+         name="YourDogsListScreen"
+         component={YourDogsListScreen}
+         options={{
+           title: 'yourDogsListScreen',
+         }}
+       />
+     </Drawer.Navigator>
+   );
+ }
+ 
+ /**
+  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+  */
+ function TabBarIcon(props: {
+   name: React.ComponentProps<typeof FontAwesome>['name'];
+   color: string;
+ }) {
+   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+ }
--- types.tsx
+++ types.tsx
@@ -3,77 +3,46 @@
  * https://reactnavigation.org/docs/typescript/
  */
 
-import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
-import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import {Dog, DogWithId} from './types/Dog';
-
-declare global {
-  namespace ReactNavigation {
-    interface RootParamList extends RootDrawerParamList {}
-    // interface RootParamList extends RootStackParamList {}
-  }
-}
-
-export type RootDrawerParamList = {
-  Login: undefined;
-// <<<<<<< Updated upstream
-//   Register: undefined;
-//   YourDogsListScreen: undefined;
-//   ForgotPassword: undefined;
-//   AddDogScreen: DogWithId | undefined;
-//   MainSwipeDogsScreen: undefined;
-//   SwipeDogsScreen: undefined;
-// };
-//
-// export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-//   RootStackParamList,
-//   Screen
-// >;
-//
-// export type RootTabParamList = {
-//   TabOne: undefined;
-//   TabTwo: undefined;
-//   YourDogsListScreen: undefined;
-//   MainSwipeDogsScreen: undefined;
-//   SwipeDogsScreen: undefined;
-//   Login: undefined;
-//   ForgotPassword: undefined;
-// };
-// =======
-  Register: undefined;
-  AddDogScreen: undefined;
-  SwipeDogsScreen: undefined;
-  YourDogsListScreen: undefined;
-  ForgotPassword: undefined;
-}
-
-// export type RootStackParamList = {
-//   Root: NavigatorScreenParams<RootTabParamList> | undefined;
-//   Modal: undefined;
-//   NotFound: undefined;
-//   Login: undefined;
-//   Register: undefined;
-//   YourDogsListScreen: undefined;
-//   ForgotPassword: undefined;
-//   AddDogScreen: DogWithId | undefined;
-// };
-//
-// export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
-//   RootStackParamList,
-//   Screen
-// >;
-//
-// export type RootTabParamList = {
-//   TabOne: undefined;
-//   TabTwo: undefined;
-//   YourDogsListScreen: undefined;
-//   Login: undefined;
-//   ForgotPassword: undefined;
-// };
-// >>>>>>> Stashed changes
-
-// export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
-//   BottomTabScreenProps<RootTabParamList, Screen>,
-//   NativeStackScreenProps<RootStackParamList>
-// >;
+ import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+ import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
+ import { NativeStackScreenProps } from '@react-navigation/native-stack';
+ import {Dog, DogWithId} from './types/Dog';
+ 
+ declare global {
+   namespace ReactNavigation {
+     interface RootParamList extends RootStackParamList {}
+   }
+ }
+ 
+ export type RootStackParamList = {
+   Root: NavigatorScreenParams<RootTabParamList> | undefined;
+   Modal: undefined;
+   NotFound: undefined;
+   Login: undefined;
+   Register: undefined;
+   YourDogsListScreen: undefined;
+   ForgotPassword: undefined;
+   AddDogScreen: DogWithId | undefined;
+   MainSwipeDogsScreen: undefined;
+   SwipeDogsScreen: undefined;
+ };
+ 
+ export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
+   RootStackParamList,
+   Screen
+ >;
+ 
+ export type RootTabParamList = {
+   TabOne: undefined;
+   TabTwo: undefined;
+   YourDogsListScreen: undefined;
+   MainSwipeDogsScreen: undefined;
+   SwipeDogsScreen: undefined;
+   Login: undefined;
+   ForgotPassword: undefined;
+ };
+ 
+ export type RootTabScreenProps<Screen extends keyof RootTabParamList> = CompositeScreenProps<
+   BottomTabScreenProps<RootTabParamList, Screen>,
+   NativeStackScreenProps<RootStackParamList>
+ >;
